48d43008d51f23d91518898fe8178223f7e5d5eb
Marwa Tawfik Badawy <67851557+MarwaTawfikBadawy@users.noreply.github.com>
2025-02-19 16:06:34 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+
 ## Attaching packages  tidyverse 1.2.1 !!
 ## ✔ ggplot2 3.1.0 ## ✔ tibble 2.0.1 ## ✔ tidyr 0.8.3 ## ✔ readr 1.3.1
 ✔ purrr 0.3.2 ✔ dplyr 0.8.0.1 ✔ stringr 1.3.1 ✔ forcats 0.4.0
@@ -8,6 +9,7 @@ library(tidyverse)
 ## Warning: package 'forcats' was built under R version 3.5.2
 ## Conflicts  tidyverse_conflicts()  ## ✖ dplyr::filter() masks stats::filter()
 ## ✖ dplyr::lag() masks stats::lag()
+
 Full_Blood = read_tsv("Marwa_Blood.tsv")
 ## Parsed with column specification:
 ## cols(
